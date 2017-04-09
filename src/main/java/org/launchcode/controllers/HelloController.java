18cac5f844cd9df5f80963942be2af38f189c5f2
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2017-04-09 17:35:03 -0500

    Added logic to demonstrate retrieving information via query parameters, post parameters, and URL segments.  Also demonstrates redirecting.

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,7 +1,9 @@
 package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,16 +14,59 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HelloController{
 
+    // To call this method - localhost:8080/?name=Chris
     @RequestMapping(value="")
     @ResponseBody
-    public String index() {
-        return "Hello World";
+    public String index(HttpServletRequest request) {
+        String name = request.getParameter("name");
+
+        if (name == null) {
+            name = "World";
+        }
+        return "Hello " + name;
     }
 
-    @RequestMapping(value="goodbye")
+    // To call this method - localhost:8080/hello
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm() {
+
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+
+        return "Hello " + name;
+    }
+
+    // To call this method - localhost:8080/hello/Chris
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+/*    @RequestMapping(value="goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye";
     }
+*/
 
+    @RequestMapping(value = "goodbye")
+    public String goodbye() {
+
+        return "redirect:/";
+        // return "redirect:/hello";
+
+    }
 }
